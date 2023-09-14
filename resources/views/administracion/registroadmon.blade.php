5cf5ed878a20b95e771ded84d46993b5e12d1138
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-09-14 01:18:09 -0600

    Desarrollo del formulario de la vista de registro administrativo

---
+++ resources/views/administracion/registroadmon.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+<div class="container mt-5">
+<div class="container mt-5">
+        <div class="row justify-content-center"> 
+            <div class="col-md-7 mt-5">
+                <!-- Mensaje flash 
+                @if(session('alumnoGuardado'))
+                <div class="alert alert-success">
+                    {{ session('alumnoGuardado') }}
+                </div>
+                @endif
+                
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif-->
+                <div class="card">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="card-header text-center">Agregar Actividad administrativa</div>
+                        <div class="card-body">
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Código de actividad:</label>
+                                <input type="text" name="codigoadmon" class="form-control col-md-9">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Nombre de actividad:</label>
+                                <input type="text" name="nombreact" class="form-control col-md-9">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Fecha:</label>
+                                <input type="date" name="fecha" class="form-control col-md-9">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Descripción</label>
+                                <textarea class="form-control" type="text" name="descripcion" rows="3"></textarea>
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Archivo o imagen</label>
+                                <input class="form-control" type="file" name="archivo">
+                            </div>
+                            </div>
+                            <div class="row form-group">
+                                <button type="submit" class="btn btn-success col-md-9 offset-2">Guardar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
