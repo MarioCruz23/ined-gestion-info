d3dcb487e44998257b8a6c1bd400dd14629b01d6
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-09-14 15:31:20 -0600

    Se implementa código para la vista de editar registro administrativo y se realiza un cambio en la vista de registro administrativo

---
+++ resources/views/administracion/editadmon.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+<div class="container mt-5">
+<div class="container mt-5">
+        <div class="row justify-content-center"> 
+            <div class="col-md-7 mt-5">
+                @if(session('actividadAdministrativoModificado'))
+                <div class="alert alert-success">
+                    {{ session('actividadAdministrativoModificado') }}
+                </div>
+                @endif
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <div class="card">
+                    <form action="{{ route('editad', $editadmon->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf @method('PATCH')
+                        <div class="card-header text-center">Modificar Actividad Administrativa</div>
+                        <div class="card-body">
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Código de actividad:</label>
+                                <input type="text" name="codigoadmon" class="form-control col-md-9" value="{{ $editadmon->codigoadmon }}">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Nombre de actividad:</label>
+                                <input type="text" name="nombreact" class="form-control col-md-9" value=" {{ $editadmon->nombreact }}">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Fecha:</label>
+                                <input type="date" name="fecha" class="form-control col-md-9" value="{{ $editadmon->fecha }}">
+                            </div>
+                            <div class="row form-group">
+                                <label form="" class="clo-2">Descripción</label>
+                                <textarea class="form-control" type="text" name="descripcion" rows="3" value="{{ $editadmon->descripcion }}"></textarea>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="clo-2">Archivo o imagen</label>
+                                <input class="form-control" type="file" name="archivo" value="{{ $editadmon->archivo }}">
+                            </div>
+                            </div>
+                            <div class="row form-group">
+                                <button type="submit" class="btn btn-success col-md-9 offset-2">Editar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/administracion/registroadmon.blade.php
+++ resources/views/administracion/registroadmon.blade.php
@@ -3,7 +3,6 @@
 <div class="container mt-5">
         <div class="row justify-content-center"> 
             <div class="col-md-7 mt-5">
-                Mensaje flash 
                 @if(session('actividadAdmonGuardado'))
                 <div class="alert alert-success">
                     {{ session('actividadAdmonGuardado') }}
