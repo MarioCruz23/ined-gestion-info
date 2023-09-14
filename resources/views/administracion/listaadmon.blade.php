445536afd4eceb1717c8cac45a3b33dee4acb167
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-09-14 12:40:04 -0600

    Se implementa el boton en la vista listaadmon para elimininar datos registrados de las actividades administrativas y un mensaje flash

--- resources/views/administracion/listaadmon.blade.php
+++ resources/views/administracion/listaadmon.blade.php
@@ -4,6 +4,11 @@
         <div class="col-md-10">
             <h2 class="text-center mb-5">Actividades administrativas Registradas</h2>
             <a class="btn btn-success mb-4" href="{{ url('/formadmon') }}">Nueva Actividad</a>
+            @if(session('actividadadmonEliminado'))
+            <div class="alert alert-success">
+                {{ session('actividadadmonEliminado') }}
+            </div>
+            @endif
             <table class="table table-bordered table-striped text-center">
                 <thead>
                     <tr>
@@ -23,6 +28,14 @@
                         <td>{{ $admon->fecha }}</td>
                         <td>{{ $admon->descripcion }}</td>
                         <td>{{ $admon->archivo }}</td>
+                        <td>
+                        <form action="{{ route('deleteadmon', $admon->id) }}" method="POST" class="Alert-eliminar">
+                            @csrf @method('DELETE')
+                            <button type="submit" onclick="return confirm('¿Seguro quiere borrar los datos de las actividades administrativas?');" class="btn btn-danger btn-block">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </form>
+                        </td>
                     </tr>
                 @endforeach
                 </tbody>
