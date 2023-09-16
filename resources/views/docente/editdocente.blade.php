ca020344430aa4dbae858f1b87f8e4ecccc69d8e
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-09-16 16:24:50 -0600

    Se realiza cambios en las vistas de gestion docente

--- resources/views/docente/editdocente.blade.php
+++ resources/views/docente/editdocente.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('content')
     <div class="container mt-5">
         <div class="row justify-content-center"> 
             <div class="col-md-7 mt-5">
@@ -71,3 +72,4 @@
             </div>
         </div>
     </div>
+@endsection
--- resources/views/docente/registro.blade.php
+++ resources/views/docente/registro.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('content')
     <div class="container mt-5">
         <div class="row justify-content-center"> 
             <div class="col-md-7 mt-5">
@@ -70,3 +71,4 @@
             </div>
         </div>
     </div>
+@endsection
