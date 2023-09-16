3f95fdcfc0a851aa776eb26227be91a811eaf270
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-09-16 16:22:46 -0600

    Se realiza cambios en las vistas de gestion alumno

--- resources/views/Alumno/lista.blade.php
+++ resources/views/Alumno/lista.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('content')
 <div class="container mt-5">
     <div class="row justify_content-center">
         <div class="col-md-10">
@@ -59,3 +60,4 @@
         </div>
     </div>
 </div>
+@endsection
