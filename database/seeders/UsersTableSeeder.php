94ae167db7e93c8be027356be5f380eff9dcb91a
MarioCruz23 <mcruzp8@miumg.edu.gt>
2023-10-27 15:32:09 -0600

    Se realiza cambios en los seeders

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,20 +18,8 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Regina Peña',
-            'email' => 'regina.pena@gmail.com',
+            'email' => 'xpena1466@gmail.com',
             'password' => Hash::make('reg12345'),
-            'role' => 'admin',
-        ]);
-        DB::table('users')->insert([
-            'name' => 'Mara',
-            'email' => 'mara.1243@gmail.com',
-            'password' => Hash::make('mara12345'),
-            'role' => 'admin',
-        ]);
-        DB::table('users')->insert([
-            'name' => 'Secretaria',
-            'email' => 'secre.1970@gmail.com',
-            'password' => Hash::make('secretaria'),
             'role' => 'admin',
         ]);
     }
